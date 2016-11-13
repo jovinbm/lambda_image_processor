378204a1c56759d3887959c6aa559e9cc220e0ea
Jovin Mwilanga <jovinbeda@hotmail.com>
2016-11-13 04:08:19 +0300

    Support for new version of aws_wrapper, image_functions

--- index.js
+++ index.js
@@ -5,22 +5,23 @@
  * - Run .config to configure the upload_folder and other settings
  */
 
-var BPromise        = require('bluebird');
-var cuid            = require('cuid');
-var fs              = require('fs');
-var path            = require('path');
-var mkdirp          = require('mkdirp');
-var rimraf          = require('rimraf');
-var aws_wrapper     = new (require('aws_wrapper').Aws_wrapper)();
-var image_functions = new (require('image_functions').image_functions)();
-var ajv             = require("ajv")({
+const BPromise       = require('bluebird');
+const cuid           = require('cuid');
+const fs             = require('fs');
+const path           = require('path');
+const mkdirp         = require('mkdirp');
+const rimraf         = require('rimraf');
+const awsWrapper     = new (require('aws_wrapper').AwsWrapper)();
+const imageFunctions = new (require('image_functions').ImageFunctions)();
+const ajv            = require('ajv')({
   removeAdditional: false
 });
-BPromise.promisifyAll(fs);
 
-var module_config = {
+const module_config = {
   upload_folder: '/tmp/images/'
 };
+
+BPromise.promisifyAll(fs);
 
 /**
  *
@@ -28,7 +29,7 @@ var module_config = {
  * @param {string} opts.upload_folder
  */
 exports.config = function (opts) {
-  var schema = {
+  const schema = {
     type                : 'object',
     additionalProperties: false,
     properties          : {
@@ -41,15 +42,17 @@ exports.config = function (opts) {
   
   return BPromise.resolve()
     .then(function () {
-      var valid = ajv.validate(schema, opts);
+      const valid = ajv.validate(schema, opts);
       
       if (!valid) {
-        var e = new Error(ajv.errorsText());
+        const e = new Error(ajv.errorsText());
+        
         e.ajv = ajv.errors;
         throw e;
       }
     })
     .then(function () {
+      
       if (opts.upload_folder) {
         module_config.upload_folder = opts.upload_folder;
       }
@@ -70,7 +73,7 @@ exports.config = function (opts) {
  */
 exports.handler = function (event, context) {
   
-  var schema = {
+  const schema = {
     type                : 'object',
     additionalProperties: false,
     required            : ['s3_key', 's3_bucket_name', 's3_output_dir'],
@@ -88,31 +91,33 @@ exports.handler = function (event, context) {
         minLength: 1
       },
       versions      : {
-        type: 'array' // see image_functions for complete format
+        type: 'array' // see imageFunctions for complete format
       }
     }
   };
   
-  var opts = {
+  const opts = {
     file_name : null,
-    input_dir : path.join(module_config.upload_folder, '/input-' + cuid() + '/'),
-    output_dir: path.join(module_config.upload_folder, '/output-' + cuid() + '/')
+    input_dir : path.join(module_config.upload_folder, `/input-${cuid()}/`),
+    output_dir: path.join(module_config.upload_folder, `/output-${cuid()}/`)
   };
   
-  var return_value = {};
+  const return_value = {};
   
   return BPromise.resolve()
     .then(function () {
       
-      var valid = ajv.validate(schema, event);
+      const valid = ajv.validate(schema, event);
       
       if (!valid) {
-        var e = new Error(ajv.errorsText());
+        const e = new Error(ajv.errorsText());
+        
         e.ajv = ajv.errors;
         throw e;
       }
       
-      console.log("lambda_process_images: Validation complete");
+      console.log('lambda_process_images: Validation complete');
+      
       return true;
       
     })
@@ -156,34 +161,36 @@ exports.handler = function (event, context) {
     })
     .then(function () {
       opts.file_name = path.basename(event.s3_key);
-      console.log("lambda_process_images: file_name = " + opts.file_name);
+      console.log(`lambda_process_images: file_name = ${opts.file_name}`);
+      
       return true;
     })
     .then(function () {
       // get the object
-      console.log("lambda_process_images: Getting object from s3");
+      console.log('lambda_process_images: Getting object from s3');
       
-      return aws_wrapper.S3_wrapper.getObject({
+      return awsWrapper.S3Wrapper.getObject({
         s3_bucket_name: event.s3_bucket_name,
         s3_key        : event.s3_key
       })
         .then(function (buffer) {
-          console.log("lambda_process_images: Got object from s3");
+          
+          console.log('lambda_process_images: Got object from s3');
           
           return fs.writeFileAsync(path.join(opts.input_dir, opts.file_name), buffer);
         });
     })
     .then(function () {
       //process
-      console.log("lambda_process_images: Starting image processing");
+      console.log('lambda_process_images: Starting image processing');
       
-      var process_opts = {
+      const process_opts = {
         dir       : opts.input_dir,
         output_dir: opts.output_dir,
         versions  : event.versions
       };
       
-      return image_functions.process_image3(process_opts);
+      return imageFunctions.processImage3(process_opts);
     })
     .then(function (resp) {
       
@@ -203,14 +210,15 @@ exports.handler = function (event, context) {
         key: path.join(event.s3_output_dir, resp[opts.file_name][0])
       };
       
-      console.log("lambda_process_images: Finished image processing");
+      console.log('lambda_process_images: Finished image processing');
+      
       return true;
     })
     .then(function () {
       
-      console.log("lambda_process_images: Uploading back to S3");
+      console.log('lambda_process_images: Uploading back to S3');
       
-      return aws_wrapper.S3_wrapper.uploadFiles({
+      return awsWrapper.S3Wrapper.uploadFiles({
         dir           : opts.output_dir,
         s3_bucket_name: event.s3_bucket_name,
         s3_output_dir : event.s3_output_dir,
@@ -218,14 +226,15 @@ exports.handler = function (event, context) {
         CacheControl  : 15552000
       })
         .then(function () {
-          console.log("lambda_process_images: Successfully uploaded back to S3");
+          console.log('lambda_process_images: Successfully uploaded back to S3');
+          
           return true;
         });
     })
     .then(function () {
       // delete the directories
       
-      console.log("lambda_process_images: Cleaning up");
+      console.log('lambda_process_images: Cleaning up');
       
       return BPromise.resolve()
         .then(function () {
@@ -257,11 +266,12 @@ exports.handler = function (event, context) {
           
         })
         .then(function () {
-          console.log("lambda_process_images: Finished cleaning up");
+          console.log('lambda_process_images: Finished cleaning up');
         });
     })
     .then(function () {
-      console.log("lambda_process_images: Returning");
+      console.log('lambda_process_images: Returning');
+      
       return context.succeed(return_value);
     });
   
